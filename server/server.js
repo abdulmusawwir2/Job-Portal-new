49764a09ce63d589f3e645c75fb96b5240977845
Abdul Musawwir <abdulmusawwir611@gmail.com>
2025-03-26 23:28:55 +0530

    second-commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
-import './config/instrument.js'
+// import './config/instrument.js'
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
-import * as Sentry from "@sentry/node";
+// import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from './controllers/webhooks.js'
 
 // initialize express
 const app = express()
-
 // connect to database
 await connectDB()
 
@@ -21,10 +20,10 @@ app.use(express.json())
 app.get('/', (req,res) => {
     res.send("Api Working")
 })
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
-});
-app.post('/webhooks',clerkWebhooks)
+// app.get("/debug-sentry", function mainHandler(req, res) {
+//   throw new Error("My first Sentry error!");
+// });
+// app.post('/webhooks',clerkWebhooks)
 
 
 //Port
