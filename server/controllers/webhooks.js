5a0a251d994d357da06f96d176110d6324cd91a6
Abdul Musawwir <abdulmusawwir611@gmail.com>
2025-03-27 17:55:21 +0530

    webhooks update commit

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,6 +1,7 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
 
+// API Controller function to manage clerk user with database
 export const clerkWebhooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
@@ -12,27 +13,28 @@ export const clerkWebhooks = async (req, res) => {
             "svix-signature": req.headers["svix-signature"],
         });
 
+        // getting data from request body
         const { data, type } = req.body;
 
         switch (type) {
             case 'user.created': {
-                const email = data.email_addresses?.[0]?.email_address || "";
                 const userData = {
                     _id: data.id,
-                    email,
-                    name: `${data.first_name} ${data.last_name}`,
+                    email:data.email_addresses[0].email_address,
+                    name: data.first_name+" "+data.last_name,
                     image: data.image_url,
-                    resume: ""
+                    resume: ''
                 };
-                await User.create(userData);
+                console.log("🔹 Saving User Data:", userData); 
+                await User.create(userData);   //save data in database
+                console.log("✅ User Data Saved");
                 res.json({ success: true });
                 break;
             }
             case 'user.updated': {
-                const email = data.email_addresses?.[0]?.email_address || "";
                 const userData = {
-                    email,
-                    name: `${data.first_name} ${data.last_name}`,
+                    email:data.email_addresses[0].email_address,
+                    name: data.first_name+" "+data.last_name,
                     image: data.image_url,
                 };
                 await User.findByIdAndUpdate(data.id, userData);
